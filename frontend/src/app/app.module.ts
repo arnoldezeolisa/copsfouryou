8db2b320aa1e06b866936993dc547ebcb8a10494
ArnoldEzeolisa <respecteasy@gmail.com>
2019-01-15 23:43:23 -0600

    bitch

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
@@ -12,10 +12,9 @@ import { SignInComponent } from './auth/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    SignUpComponent,
+    AppComponent,    SignUpComponent,
     SignInComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule
