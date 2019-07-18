91b4f1dbf031a459087333966455ca88a01f57c4
ArnoldEzeolisa <respecteasy@gmail.com>
2019-07-17 21:41:42 -0500

    profileplay

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+ import { Component, OnInit, OnDestroy } from '@angular/core';
+ import { Subscription } from 'rxjs/Subscription';
 
-@Component({
+ import { Profile } from "./profile.model";
+ import {ProfileService } from "./profile.service";
+
+ @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+   profiles: Profile[];
+   private subscription: Subscription;
 
-  constructor() { }
+  constructor(private  prService: ProfileService) { }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+     this.profiles = this.prService.getProfiles();
+     this.subscription = this.prService.profileChanged
+       .subscribe(
+         (ingredients: Profile[]) => {
+           this.profiles = ingredients;
+         }
+       );
+   }
 
-}
+
+ onEditItem(index: number) {
+   this.prService.startedEditing.next(index);
+ }
+
+ ngOnDestroy() {
+   this.subscription.unsubscribe();
+ }
+ }
